88c8b31146ff5ed4602ff52fcb8d09e0a5d255d3
Abhishek4848 <54174670+Abhishek4848@users.noreply.github.com>
2020-03-27 01:36:35 +0530

    Implemented shared_preferences to check the login state of user.

--- lib/sign_in.dart
+++ lib/sign_in.dart
@@ -1,8 +1,30 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 final FirebaseAuth _auth = FirebaseAuth.instance;
 final GoogleSignIn googleSignIn = GoogleSignIn();
+bool authSignedIn;
+String uid;
+String name;
+String email;
+String imageUrl;
+
+Future getUser() async {
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  authSignedIn = prefs.getBool('auth') ?? false;
+
+  final FirebaseUser user = await _auth.currentUser();
+
+  if (authSignedIn == true) {
+    if (user != null) {
+      uid = user.uid;
+      name = user.displayName;
+      email = user.email;
+      imageUrl = user.photoUrl;
+    }
+  }
+}
 
 Future<String> signInWithGoogle() async {
   final GoogleSignInAccount googleSignInAccount = await googleSignIn.signIn();
@@ -17,17 +39,33 @@ Future<String> signInWithGoogle() async {
   final AuthResult authResult = await _auth.signInWithCredential(credential);
   final FirebaseUser user = authResult.user;
 
+  assert(user.email != null);
+  assert(user.displayName != null);
+  assert(user.photoUrl != null);
+
+  name = user.displayName;
+  email = user.email;
+  imageUrl = user.photoUrl;
+
   assert(!user.isAnonymous);
   assert(await user.getIdToken() != null);
 
   final FirebaseUser currentUser = await _auth.currentUser();
   assert(user.uid == currentUser.uid);
 
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  prefs.setBool('auth', true);
+  authSignedIn = true;
+
   return 'signInWithGoogle succeeded: $user';
 }
 
 void signOutGoogle() async{
   await googleSignIn.signOut();
+  
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  prefs.setBool('auth', false);
+  authSignedIn = false;
 
   print("User Sign Out");
 }
